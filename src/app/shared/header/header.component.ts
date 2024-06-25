624d34392a0efbd0b17784e9b791c294c8ee82bb
panchal aditya nileshbhai <adityajsn1299@gmail.com>
2024-06-25 22:50:22 +0530

    chore: Update header component CSS and HTML
    This commit updates the header component CSS and HTML files.
    
    In the CSS file, the following changes were made:
    - Added styles for text alignment in different screen sizes.
    
    In the HTML file, the following changes were made:
    - Commented out the video element and image element.
    
    These changes were made to improve the layout and alignment of the header component.

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-dhyaniTechLogo = 'DhyaniTechLogo.png';
+export class HeaderComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+    // document.getElementById('nav-toggle').onclick = function () {
+    // docum/ent.getElementById("nav-content").classList.toggle("hidden");
+    // }
+  }
+  dhyaniTechLogo = 'DhyaniTechLogo.png';
 }
+
